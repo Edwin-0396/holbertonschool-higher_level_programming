db7426cfac8d6c9e23bd8957187ee71f8f1091b9
Edwin-9603 <egc4611@gmail.com>
2022-05-03 14:29:01 -0500

    new python project

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(10):
-	for j in range(10):
-		if (i!=j):
-			print(f"{i}{j}", end=', ')
-print()
+    for j in range(10):
+        if (j > i and i < 8):
+            print(f"{i:d}{j:d}", end=', ')
+print(89)
