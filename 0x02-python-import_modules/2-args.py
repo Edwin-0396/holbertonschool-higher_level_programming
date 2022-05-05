3686cca096452114e9b37dd67a19799abe43cb92
Edwin-9603 <egc4611@gmail.com>
2022-05-05 14:26:10 -0500

    program that prints the number of and the list of its arguments.

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+import sys
+
+Number_of_arguments = len(sys.argv)
+if Number_of_arguments <= 1:
+	print(f"0 arguments.")
+else:
+	if len(sys.argv) == 2:
+		print(f"{len(sys.argv) - 1} argument:\n{len(sys.argv) - 1}: {sys.argv[1]}")
+	elif len(sys.argv) > 2:		
+		print(f"{len(sys.argv) - 1} arguments:")
+		for i in range(1, len(sys.argv)):
+			print(f"{i}: {sys.argv[i]}")
+			
