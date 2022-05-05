1af98ee3b9402283fe93992f590e2fc9bb5757c6
Edwin-9603 <egc4611@gmail.com>
2022-05-05 15:24:49 -0500

    program that prints the result of the addition of all arguments

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 if __name__ == '__main__':
-	import sys
+    import sys
 
-	addition_of_arguments = 0
-	for i in range(1, len(sys.argv)):
-		addition_of_arguments += int(sys.argv[i])
-	print(addition_of_arguments)
+    addition_of_arguments = 0
+    for i in range(1, len(sys.argv)):
+        addition_of_arguments += int(sys.argv[i])
+    print(addition_of_arguments)
