7dd61e50710481d7223df3d50f5d1c556a410b5e
Edwin-9603 <egc4611@gmail.com>
2022-05-09 16:54:31 -0500

    function that replaces an element in a list at a specific position without modifying the original list (like in C).

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,12 +1,6 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-	MY_LIST = my_list
-	if idx < 0:
-		return MY_LIST
-	if idx >= len(my_list):
-		return MY_LIST
-	for x in range(len(MY_LIST)):
-		if x == idx:
-			MY_LIST[x] = element
-			return MY_LIST
-	
+    new_list = my_list[:]
+    if idx >= 0 and idx < len(my_list):
+        new_list[idx] = element
+    return new_list
