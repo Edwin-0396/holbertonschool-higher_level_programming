78582e7759db526678eac5076e27659c77090f12
Edwin-9603 <egc4611@gmail.com>
2022-05-23 17:56:34 -0500

    Python -exceptions

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,7 +5,7 @@ def safe_print_list(my_list=[], x=0):
         for i in range(x):
             print(my_list[i], end="")
             count += 1
-    except:
+    except Exception:
         pass
     print()
     return count
