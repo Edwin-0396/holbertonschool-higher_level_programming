abffd1e960cc3e8868a355e7b2077517eccee2dd
Edwin-9603 <egc4611@gmail.com>
2022-05-24 18:54:00 -0500

    python - classes

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,36 +1,35 @@
 #!/usr/bin/python3
-"""Module that creates a class named square"""
+"""This module creates a class named Square"""
 
 
 class Square:
-    """A class named square
+    """A class named Square
     Attributes:
     attr1 (size): size of square
     """
-
     def __init__(self, size=0, position=(0, 0)):
         """
         Args:
         size (int): size for __size attribute of class instance
         """
-        self.__size = size
-        self__position = position
+        self.size = size
+        self.position = position
 
     def area(self):
-        """Determine the area of the square
+        """Calculates the area based on size of square
         Returns:
-        int: The size of the square
+        int: The return value. Returns the area
         """
         return self.__size ** 2
 
     @property
     def size(self):
-        """Get the size of the class"""
+        """Gets the size of the class instance"""
         return self.__size
 
     @size.setter
     def size(self, value):
-        """Set the size of the class"""
+        """Sets the size of the class instance"""
         if type(value) != int:
             raise TypeError("size must be an integer")
         if value < 0:
@@ -38,14 +37,14 @@ class Square:
         self.__size = value
 
     def my_print(self):
-        """Prints in stdout the square with #"""
+        """Prints in stdout the square with the # character"""
         if self.__size > 0:
             for row in range(self.__position[1]):
                 print()
             for row in range(self.__size):
                 for column in range(self.__position[0]):
                     print(" ", end="")
-                for colum in range(self.__size):
+                for column in range(self.__size):
                     print("#", end="")
                 print()
         else:
@@ -53,15 +52,15 @@ class Square:
 
     @property
     def position(self):
-        """get the position of the square"""
+        """Gets the position of the square"""
         return self.__position
 
     @position.setter
     def position(self, value):
-        """Set the position of the square"""
+        """Sets the position of the square"""
         if type(value) != tuple or len(value) != 2:
             raise TypeError("position must be a tuple of 2 positive integers")
-        if type(value[0] != int or type(value[1] != 1)):
+        if type(value[0]) != int or type(value[1]) != int:
             raise TypeError("position must be a tuple of 2 positive integers")
         if value[0] < 0 or value[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
