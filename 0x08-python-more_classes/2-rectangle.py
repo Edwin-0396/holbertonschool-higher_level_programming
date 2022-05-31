5d8707e124e05c5c0e065f92cad37166c1b84f41
Edwin-9603 <egc4611@gmail.com>
2022-05-31 18:40:39 -0500

    more classes and objects

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -11,8 +11,8 @@ class Rectangle:
 
     def __init__(self, width=0, height=0):
         """Class instance"""
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
         Rectangle.number_of_instances += 1
 
     @property
