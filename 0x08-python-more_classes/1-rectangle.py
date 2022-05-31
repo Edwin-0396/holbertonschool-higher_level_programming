12ffdba9d824d95def01d3067d4d71b7cb50dea8
Edwin-9603 <egc4611@gmail.com>
2022-05-31 18:37:51 -0500

    more classes and objects

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -10,7 +10,6 @@ class Rectangle:
         """Class instance"""
         self.__width = width
         self.__height = height
-        Rectangle.number_of_instances += 1
 
     @property
     def width(self):
