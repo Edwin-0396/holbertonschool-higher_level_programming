78dfd39a88e4153487fa037cf50f63294555a2d8
Edwin-9603 <egc4611@gmail.com>
2022-06-07 18:56:27 -0500

    new inheritance proyect

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Module to see if instance of child class"""
 
+
 def inherits_from(obj, a_class):
     """Returns true if instance of the inherited class"""
     return (isinstance(obj, a_class) and type(obj) != a_class)
