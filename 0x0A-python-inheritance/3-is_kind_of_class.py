e0303de444fa367b349c5d4511a23a67ad2fa4ea
Edwin-9603 <egc4611@gmail.com>
2022-06-07 18:51:21 -0500

    new inheritance proyect

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Module to see the kind of an obj"""
 
+
 def is_kind_of_class(obj, a_class):
     """Object an instance of the a_class as inherited class"""
     return (isinstance(obj, a_class))
