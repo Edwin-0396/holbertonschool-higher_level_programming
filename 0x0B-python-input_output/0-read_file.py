f7db6aab0b4080b251ca8913ee9b4dba1577f9cb
Edwin-9603 <egc4611@gmail.com>
2022-06-08 17:13:45 -0500

    Input/Output

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""function that reads a text file (UTF8) and prints it to stdout:"""
+
+def read_file(filename=""):
+	"""Reads a text file and prints it to stdout
+    Args:
+    filename (str): Filename
+    """
+	with open(filename, encoding="utf-8") as f:
+		read_data = f.read()
+	print(read_data)
