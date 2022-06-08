627f84d9334fa023b819c43018e118b9b5079c17
Edwin-9603 <egc4611@gmail.com>
2022-06-08 17:20:59 -0500

    write files

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 """This module contain the write function"""
 
+
 def write_file(filename="", text=""):
-	"""This function writes into text file
-	Args:
-	filename: file to wirte to
-	text: text to write
-	"""
-	with open(filename,'w', encoding="utf-8") as f:
-		return f.write(text)
+    """This function writes into text file
+    Args:
+    filename: file to wirte to
+    text: text to write
+    """
+    with open(filename, 'w', encoding="utf-8") as f:
+        return f.write(text)
