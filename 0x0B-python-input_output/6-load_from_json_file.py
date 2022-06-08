a6a420eb9b7a92618ed77a98d27fa334e0f0e412
Edwin-9603 <egc4611@gmail.com>
2022-06-08 18:57:30 -0500

    get object from json

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""module with function to get object from json file"""
+
+import json
+
+
+def load_from_json_file(filename):
+    """get object form json file
+    Args:
+    filename: file to get from
+    """
+
+    with open(filename, 'w', encoding='utf-8') as file:
+        return json.loads(file.read())
