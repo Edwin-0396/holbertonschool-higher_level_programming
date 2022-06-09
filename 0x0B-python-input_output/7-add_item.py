cf1e9234553a7289b4cedc448d3a034ab12e59d3
Edwin-9603 <egc4611@gmail.com>
2022-06-08 19:15:14 -0500

    add to a json file

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""Adds python args to a list and save them"""
+from os import path
+from sys import argv
+
+save_to_json_file = __import__('5-save_to_json_file.py').save_to_json_file
+load_from_json_file = __import__(
+    '6-load_from_json_file.py').load_from_json_file
+
+if path.exists("add_item.json") is False:
+    save_to_json_file([], "add_item.json")
+my_list = load_from_json_file("add_item.json")
+for i in range(1, len(argv)):
+    my_list.append(argv[i])
+save_to_json_file(my_list, "add_item.json")
