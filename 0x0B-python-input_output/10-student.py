e6194c9ba166c01ecb0a58e10ee07eca63ccb2a8
Edwin-9603 <egc4611@gmail.com>
2022-06-08 23:21:39 -0500

    new 10 task

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+"""Module that defines a class student"""
+
+
+class Student:
+    """"Class named Student
+
+    Attributes:
+    first name: first name of student
+    last name: last name of student
+    age: age of student
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """Instance attributes"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Dictionary represetation of student"""
+        return self.__dict__
