3c55420c20cebc71e5981d69b446eb531ce43ba3
Edwin-9603 <egc4611@gmail.com>
2022-06-14 17:18:39 -0500

    new almost circle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -5,7 +5,16 @@ from models.base import Base
 
 
 class Rectangle(Base):
+	"""A class named Rectangle
+	Attributes:
+	attr1(id): id of object
+	attr2(width): rectangle width
+	attr3(height): rectangle height
+	attr4(x): number of spaces before rectangle
+	attr5(y): number of newlines before rectangle
+	"""
 	def __init__(self, width, height, x=0, y=0, id=None):
+		"""Initializes instance of Rectangle class"""
 		self.staticfunc(width, "width")
 		self.staticfunc(height, "height")
 		self.staticfunc(x, "x")
@@ -18,6 +27,7 @@ class Rectangle(Base):
 
 	@staticmethod
 	def staticfunc(value, name_met):
+		"""staticfunc to manage the errors"""
 		if type(value) is not int:
 			raise TypeError(f"{name_met} must be an integer")
 		if value <= 0 and name_met in ("width", "height"):
@@ -26,9 +36,11 @@ class Rectangle(Base):
 			raise ValueError(f"{name_met} must be >= 0")
 
 	def area(self):
+		"""Returns the area value of the Rectangle instance"""
 		return self.__width * self.__height
 
 	def display(self):
+		"""Prints in stdout the instance with the character #"""
 		if self.__y > 0:
 			for i in range(self.__y):
 				print()
@@ -41,24 +53,29 @@ class Rectangle(Base):
 			print()
 
 	def __str__(self):
+		"""Returns the informal representation of the object"""
 		return f"[Rectangle] ({self.id}) {self.x}/\
 	{self.y} - {self.width}/{self.height}"
 
 	@property
 	def width(self):
+		"""Returns width of instance"""
 		return self.__width
 
 	@width.setter
 	def width(self, value):
+		"""Sets width of class instance"""
 		self.staticfunc(value, "width")
 		self.__width = value
 
 	@property
 	def height(self):
+		"""Returns height of instance"""
 		return self.__height
 
 	@height.setter
 	def height(self, value):
+		"""Sets height of class instance"""
 		self.staticfunc(value, "height")
 		self.__height = value
 
@@ -68,22 +85,26 @@ class Rectangle(Base):
 
 	@x.setter
 	def x(self, value):
+		"""Returns x of instance"""
 		self.staticfunc(value, "x")
 		self.__x = value
 
 	@property
 	def y(self):
+		"""Returns y of instance"""
 		return self.__y
 
 	@y.setter
 	def y(self, value):
+		"""Sets y of class instance"""
 		self.staticfunc(value, "y")
 		self.__y = value
 
 	def update(self, *args, **kwargs):
+		"""Updates attributes"""
 		if len(args) == 0:
 			for key in kwargs:
-				setattr(self, key, kwargs[key]) 
+				setattr(self, key, kwargs[key])
 
 		else:
 			tupla = [
@@ -97,4 +118,5 @@ class Rectangle(Base):
 				setattr(self, tupla[i], args[i])
 
 	def to_dictionary(self):
+		"""Returns the dictionary representation of the Rectangle"""
 		return vars(self)
