5df6c9545bec4ac6a356a99fc52f74819ef526e1
Edwin-9603 <egc4611@gmail.com>
2022-06-14 19:48:36 -0500

    New 20 task

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -72,3 +72,28 @@ class Base:
                     objectdict[key] = value
                 listofinstances.append(cls.create(**objectdict))
             return listofinstances
+
+    @classmethod
+    def save_to_file_csv(cls, list_objs):
+        """ JSON string to file"""
+
+        filename = cls.__name__ + ".csv"
+        if list_objs is not None:
+            list_objs = [i.to_dictionary() for i in list_objs]
+        with open(filename, "w") as f:
+            f.write(cls.to_json_string(list_objs))
+
+    @classmethod
+    def load_from_file_csv(cls):
+        """loads a list of instances from a json file"""
+        if path.exists(cls.__name__ + ".csv") is False:
+            return []
+        with open(cls.__name__ + ".csv", "r",  encoding='utf-8') as file:
+            listofinstances = []
+            objectlist = cls.from_json_string(file.read())
+            for dict in objectlist:
+                objectdict = {}
+                for key, value in dict.items():
+                    objectdict[key] = value
+                listofinstances.append(cls.create(**objectdict))
+            return listofinstances
