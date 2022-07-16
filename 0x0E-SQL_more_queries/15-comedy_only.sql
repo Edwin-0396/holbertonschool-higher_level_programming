05ab3b90eb174192d71910b23e0877aa1865a259
Edwin-9603 <egc4611@gmail.com>
2022-07-16 00:07:10 -0400

    New more queries - task 15

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,6 +1,6 @@
 -- Write a script that uses the hbtn_0d_tvshows database to lists all genres of the show Dexter
-SELECT tv_shows.title FROM tv_shows
+SELECT tv_shows.title AS title FROM tv_shows
 JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
-WHERE tv_genres.name = 'Comedy'
-ORDER BY tv_shows.title ASC;
+WHERE tv_genres.name = "Comedy"
+ORDER BY tv_shows.title
