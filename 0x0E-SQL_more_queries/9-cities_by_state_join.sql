59c8943405abd0d8c8d22e9eb224dca2c2cfba8b
Edwin-9603 <egc4611@gmail.com>
2022-07-16 09:39:33 -0400

    Task 9 - number of citites

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,3 +1,3 @@
 -- Write a script that lists all cities contained in the database hbtn_0d_usa.
 SELECT cities.id, cities.name, states.name FROM cities 
-JOIN states ON cities.id = states.id ORDER BY cities.id ASC;
+JOIN states ON cities.state_id = states.id ORDER BY cities.id ASC;
