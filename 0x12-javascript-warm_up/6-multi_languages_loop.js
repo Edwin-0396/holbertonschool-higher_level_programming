8a3f0f3c8af39364b2c35b99bb09f74b7f0aa617
Edwin-9603 <egc4611@gmail.com>
2022-08-16 11:33:03 -0400

    new updates js warm up

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const strings = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-for (const string in strings) {
-  console.log(strings[string]);
+const arr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (let i = 0; i < arr.length; i++) {
+  console.log(arr[i]);
 }
