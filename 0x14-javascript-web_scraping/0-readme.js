53cc7a3ce58c39d07156732ee3de7ac100de7f69
Edwin-9603 <egc4611@gmail.com>
2022-08-17 19:55:46 -0400

    new updates and fixes

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,8 +2,8 @@
 const fs = require('fs');
 
 fs.readFile(process.argv[2], 'utf-8', (err, data) => {
-    if (err) {
-        throw err;
-    }
-    console.log(data);
+  if (err) {
+    throw err;
+  }
+  console.log(data);
 });
--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 
 // write JSON string to a file
 fs.writeFile(process.argv[2], process.argv[3], 'utf-8', (err) => {
-    if (err) {
-        throw err;
-    }
+  if (err) {
+    throw err;
+  }
 });
