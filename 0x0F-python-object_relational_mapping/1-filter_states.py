a728e0a765a0bfcaaa08a3fa17cd5f6c4ef05249
Edwin-9603 <egc4611@gmail.com>
2022-08-30 17:05:47 -0400

    new updates

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" script that lists all states from the database hbtn_0e_0_usa: """
+
+
+if __name__ == '__main__':
+    """ Module to select states"""
+
+    import MySQLdb
+    import sys
+
+    db = MySQLdb.connect(
+        host='localhost', user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    mycursor = db.cursor()
+
+    sql = "SELECT * FROM states where states.name LIKE 'N%' ORDER\
+BY states.id ASC"
+
+    mycursor.execute(sql)
+    myresult = mycursor.fetchall()
+
+    for x in myresult:
+        print(x)
