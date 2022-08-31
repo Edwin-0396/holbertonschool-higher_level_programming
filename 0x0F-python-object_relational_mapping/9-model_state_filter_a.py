92576389913742db977c90e889a3ce2c536eec98
Edwin-9603 <egc4611@gmail.com>
2022-08-31 18:49:30 -0400

    new updates

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -5,19 +5,19 @@
 
 
 if __name__ == "__main__":
-	from model_state import State
-	from sqlalchemy import create_engine
-	from sqlalchemy.orm import Session
-	import sys
+    from model_state import State
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import Session
+    import sys
 
-	engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}".format(
-		sys.argv[1], sys.argv[2], sys.argv[3]))
-	engine.connect()
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}".format(
+        sys.argv[1], sys.argv[2], sys.argv[3]))
+    engine.connect()
 
-	session = Session(engine)
+    session = Session(engine)
 
-	for state in session.query(State).order_by(State.id).all():
-		if "a" in state.name:
-			print("{}: {}".format(state.id, state.name))
+    for state in session.query(State).order_by(State.id).all():
+        if "a" in state.name:
+            print("{}: {}".format(state.id, state.name))
 
-	session.close()
+    session.close()
