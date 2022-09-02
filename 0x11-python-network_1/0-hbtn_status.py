92ebfb8fa79b104deb606461eef9fc575377e7a0
Edwin-9603 <egc4611@gmail.com>
2022-09-02 11:25:22 -0400

    new updats

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,7 +2,6 @@
 """script that fetches https://intranet.hbtn.io/status """
 
 if __name__ == "__main__":
-
     import urllib.request
 
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
