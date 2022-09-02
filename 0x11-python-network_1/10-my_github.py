1fe323be1342b014086b4c4ac7aab5fe31e94b92
Edwin-9603 <egc4611@gmail.com>
2022-09-02 12:38:48 -0400

    new updates

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" script that takes in a URL, sends a request to the
+URL and displays the body of the response"""
+
+if __name__ == "__main__":
+    import sys
+    import requests
+    from requests.auth import HTTPBasicAuth
+
+    basic = HTTPBasicAuth(sys.argv[1], sys.argv[2])
+    req = requests.get('https://docs.github.com/en/rest/users', auth=basic)
+    req_dict = req.json()
+    print(req_dict.get('id'))
