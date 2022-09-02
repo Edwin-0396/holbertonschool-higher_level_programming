427140cdd149179bafb4ad29cbd805659831b563
Edwin-9603 <egc4611@gmail.com>
2022-09-02 11:13:18 -0400

    new updats

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,10 +2,11 @@
 """ script that fetches https://intranet.hbtn.io/status"""
 
 if __name__ == "__main__":
-    import urllib.request
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-        html = response.read()
-        print("Body response:")
-        print(f"\t- type: {type(html)}")
-        print(f"\t- content: {html}")
-        print(f"\t- utf8 content: {html.decode('utf-8')}")
+	""" script that fetches https://intranet.hbtn.io/status"""
+	import urllib.request
+	with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+		html = response.read()
+		print("Body response:")
+		print(f"\t- type: {type(html)}")
+		print(f"\t- content: {html}")
+		print(f"\t- utf8 content: {html.decode('utf-8')}")
