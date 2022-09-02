8cde1e2c4daed79977069e7149e4f0e3e2c9a354
Edwin-9603 <egc4611@gmail.com>
2022-09-02 13:43:46 -0400

    new updates

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 #script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
-
 curl -sH "X-HolbertonSchool-User-Id: 98" "$1"
--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 #script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
-
 curl -sX POST -d "email=test@gmail.com" -d "subject=I will always be here for PLD" "$1"
