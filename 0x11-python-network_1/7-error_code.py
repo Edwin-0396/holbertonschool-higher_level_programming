dbd462b058050c6a41c776b50e6ee1a126825b04
Edwin-9603 <egc4611@gmail.com>
2022-09-02 12:14:08 -0400

    new updates

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -8,6 +8,6 @@ if __name__ == "__main__":
 
     response = req.get(sys.argv[1])
     if response.status_code >= 400:
-        print(f"Error code: {response.status_code}")
+        print("Error code: {}".format(response.status_code))
     else:
         print(response.text)
